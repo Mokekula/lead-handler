80bc66314535c847d9d94d073cb0690c564cece8
Mokekula <dmitriy.sedykh33@gmail.com>
2026-03-14 22:37:53 +0400

    activated ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 import type { Env } from './common/config/env.validation';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = app.get(ConfigService<Env, true>);
 
   app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
   await app.listen(config.get('PORT'));
 }
 bootstrap();
